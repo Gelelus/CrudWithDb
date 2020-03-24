eec175e9ed0e3040b0f6e024de1dd997282a3ea8
Uri <47351041+Gelelus@users.noreply.github.com>
2020-03-24 16:15:18 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = require('./routers/export-router');
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+
+require('dotenv').config(); //прочтение env
+
+mongoose.connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+  
+  console.log(
+            `
+           State of connection - ${mongoose.connection.readyState} 
+           0 = disconnected
+           1 = connected
+           2 = connecting 
+           3 = disconnecting
+        `
+          );
+
+
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use('/users', router.userRouter)
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+    console.log('server on port ' + port)
+})
