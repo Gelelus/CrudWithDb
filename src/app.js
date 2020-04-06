644807dec12c90353f012395fd5259fcec7bf008
Uri <47351041+Gelelus@users.noreply.github.com>
2020-04-06 10:32:13 +0300

    nothing

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_DB, {
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-  
+    
   console.log(
             `
            State of connection - ${mongoose.connection.readyState} 
